e6dc0cfd309f51393466699fe562ea714d73f9d8
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-03-08 11:49:59 -0300

    Autenticando Usuario no Banco de dados

--- src/main/java/br/adsweb/dao/UsuarioDAO.java
+++ src/main/java/br/adsweb/dao/UsuarioDAO.java
@@ -1,10 +1,41 @@
 package br.adsweb.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.adsweb.exception.PersistenciaException;
+import br.adsweb.util.ConexaoUltil;
+
+// Class Dao de acesso a dados dos Usuarios
+
 public class UsuarioDAO {
 	
-	public boolean validarUsuario(String usuario, String senha ) {
+	// Methodo de autenticação do usuario 
+	public boolean validarUsuario(String usuario, String senha ) throws PersistenciaException {
 		
-		return false;
+		try {
+			Connection connect = ConexaoUltil.getConexao();
+			
+			StringBuilder sql = new StringBuilder();
+			sql.append("select * from tb_usuario");
+			sql.append(" where usuario = ? and senha = ?");
+			
+			PreparedStatement pre = connect.prepareStatement(sql.toString());
+			
+			pre.setString(1, usuario);
+			pre.setString(2, senha);
+			
+			ResultSet resul = pre.executeQuery();
+			
+			return resul.next();
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			throw new PersistenciaException(e);
+		}
+				
 	}
 
 }
---
+++ src/main/java/br/adsweb/exception/PersistenciaException.java
@@ -0,0 +1,22 @@
+package br.adsweb.exception;
+
+// Encapsular todas as exceções de banco de dados
+
+public class PersistenciaException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public PersistenciaException(String erro) {
+		super(erro);
+	}
+	
+	public PersistenciaException(Exception e ) {
+		super(e);
+	}
+	
+	public PersistenciaException(String erro, Exception e ) {
+		super(erro, e);
+	}
+
+}
