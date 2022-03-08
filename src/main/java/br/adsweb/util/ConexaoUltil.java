615fb1ab3fa1d3ae885b40e83eaa2c12dc417f6d
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-03-08 00:09:42 -0300

    Criando Conexão com Banco de Dados Mysql

---
+++ src/main/java/br/adsweb/util/ConexaoUltil.java
@@ -0,0 +1,32 @@
+package br.adsweb.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+// classe de conexao SQL
+
+public class ConexaoUltil {
+	
+	private static ResourceBundle configBD = ResourceBundle.getBundle(Constantes.CONEXAO_DB_PROPERTIES);
+	
+	public static Connection getConexao() throws SQLException, ClassNotFoundException {
+		Class.forName(configBD.getString(Constantes.CONEXAO_DB_DRIVER));
+		
+		return DriverManager.getConnection(configBD.getString(Constantes.CONEXAO_DB_URL), 
+				configBD.getString(Constantes.CONEXAO_DB_USER), configBD.getString(Constantes.CONEXAO_DB_PASSWORD));
+		
+	}
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println(getConexao());
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/br/adsweb/util/Constantes.java
@@ -0,0 +1,18 @@
+package br.adsweb.util;
+
+//Classe Universais para o sistema
+
+public class Constantes {
+
+	public static final String CONEXAO_DB_PROPERTIES = "br.adsweb.util.config_bd";
+	
+	public static final String CONEXAO_DB_DRIVER =  "conexao.driver.mysql";
+	
+	public static final String CONEXAO_DB_URL =  "conexao.url";
+	
+	public static final String CONEXAO_DB_USER = "conexao.user";
+	
+	public static final String CONEXAO_DB_PASSWORD = "conexao.password"; 
+			
+			
+}
